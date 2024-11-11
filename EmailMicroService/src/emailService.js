06c677952f1632f553630fd42c77937e757fae91
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-11-11 18:32:52 -0300

    Add: chnage password integration

--- EmailMicroService/src/emailService.js
+++ EmailMicroService/src/emailService.js
@@ -8,6 +8,7 @@ config()
 export class EmailService {
 
     #userAuth;
+    #transport;
     constructor() {
         const REDIRECT_URI = "https://developers.google.com/oauthplayground"
 
@@ -17,6 +18,8 @@ export class EmailService {
             REDIRECT_URI)
 
         this.#userAuth.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
+        this.#configTransport()
+
     }
 
 
@@ -104,7 +107,7 @@ export class EmailService {
         <h1>Account Confirmation</h1>
         <p>Hello ${user.userName}</p>
         <p>Congratulations on signing up for Just Translate. <br> Click the button below to confirm your new account:</p>
-        <form id="emailForm" action="http://localhost:8729/user/${user.userId}/confirm" method="GET">
+        <form id="emailForm" action="http://localhost:8729/api/email/user/${user.userId}/confirm" method="GET">
             <button type="submit" class="btn-send">Send</button>
         </form>
         <p>If you have already confirmed your account, please ignore this email.</p>
@@ -115,36 +118,119 @@ export class EmailService {
     </div>
 </body>
 </html>
-`
-
-            const accessToken = await this.#userAuth
-                .getAccessToken();
-
-            const transport = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    type: 'OAuth2',
-                    user: "rissardi.luiz2006@gmail.com",
-                    password: "Fabio24031981",
-                    clientId: process.env.CLIENT_ID,
-                    clientSecret: process.env.CLIENT_SECRET,
-                    refreshToken: process.env.REFRESH_TOKEN,
-                    accessToken: accessToken,
-                },
-            });
+            `;
 
             const mailOptions = {
                 to: "rissardi.luiz2006@gmail.com",
-                subject: 'Hello from gmail using API',
-                text: 'Hello from gmail email using API',
+                subject: 'Confirm your account in Just Translate',
                 html: html,
             };
 
-            const result = await transport.sendMail(mailOptions);
+            const result = await this.#transport.sendMail(mailOptions);
             return result;
         } catch (error) {
             console.log(error);
         }
+    }
+
+    async sendChangePasswordEmail(emailUser) {
+        try {
+            const html = `
+            <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Confirmation</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f8ff;
+                color: #333;
+                margin: 0;
+                padding: 20px;
+            }
+    
+            .container {
+                max-width: 600px;
+                margin: auto;
+                background-color: #ffffff;
+                border-radius: 8px;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                padding: 30px;
+            }
+    
+            h1 {
+                color: #007bff;
+                text-align: center;
+            }
+    
+            p {
+                font-size: 16px;
+                line-height: 1.5;
+                text-align: center;
+            }
+
+            .footer {
+                text-align: center;
+                font-size: 12px;
+                color: #777;
+                margin-top: 20px;
+            }
+
+    
+        </style>
+    </head>
+    
+    <body>
+        <div class="container">
+            <h1> Change the password </h1>
+            <p>Hello Friend</p>
+            <p> Click 
+            <a href="http://localhost:4200/${emailUser}/recover/password"> 
+                Here
+            </a> to redirect and change password </p>
+            <br/>
+            <p>If you have already change the password, please ignore this email.</p>
+            <p>Thank you!</p>
+        </div>
+        <div class="footer">
+            <p>&copy; 2024 Just Translate. All rights reserved.</p>
+        </div>
+    </body>
+
+            </html>`
+
+            const mailOptions = {
+                to: "rissardi.luiz2006@gmail.com",
+                subject: 'Change your password in Just Translate',
+                html: html,
+            };
+
+            const result = await this.#transport.sendMail(mailOptions);
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    async #configTransport() {
+        const accessToken = await this.#userAuth.getAccessToken()
+
+        this.#transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: "rissardi.luiz2006@gmail.com",
+                password: process.env.EMAIL_APP_PASSWORD,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+        });
     }
 
 }
--- EmailMicroService/src/server.js
+++ EmailMicroService/src/server.js
@@ -13,11 +13,13 @@ const emailService = new EmailService();
 app.register(fastifyHelmet);
 app.register(fastifyCors, {
     // origin: 'https://www.teste/domain', // Permite todos os domínios, ajuste conforme necessário
-    methods: ['GET'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization',"IS-CONFIRMATED"], // Cabeçalhos permitidos
+    methods: ['GET','POST'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', "IS-CONFIRMATED"], // Cabeçalhos permitidos
 });
 
 // Rotas
+
+// rota para enviar a confirmação de conta
 app.post("/api/email/confirmation/:userId", async (req, reply) => {
     const user = req.body;
     user.userId = req.params.userId;
@@ -32,18 +34,33 @@ app.post("/api/email/confirmation/:userId", async (req, reply) => {
     return reply
 })
 
-app.get("/user/:userId/confirm", async (req, reply) => {
+// rota para enviar a troca de senha
+app.post("/api/email/change-password", async (req, reply) => {
+    const { email } = req.body;
+    if (email != undefined) {
+        const result = await emailService.sendChangePasswordEmail(email);
+        if (result?.accepted.length >= 1) {
+            reply.send({ isSuccess: true })
+        } else {
+            reply.send({ isSuccess: false })
+        }
+    }
+    return reply
+})
+
+// rota para auxiliar a confirmação de conta 
+app.get("/api/email/user/:userId/confirm", async (req, reply) => {
     const userId = req.params.userId;
 
     const data = await fetch(`http://localhost:3000/user/${userId}/confirm`, {
-        method: 'PATCH', 
+        method: 'PATCH',
         headers: {
             'XXX-CONFIRM-A-TOKEN': process.env.XXX_CONFIRM_A_TOKEN  // Cabeçalho personalizado
         }
     })
 
     const result = await data.json();
-    if(result.isSuccess){
+    if (result.isSuccess) {
         reply.redirect(`http://localhost:4200/auth/sign-in`);
     }
 })
