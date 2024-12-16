adadd3a94aaeaf6bb3911a5d06c0246b1db287cc
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-12-15 21:13:13 -0300

    ADD: FINALS

--- translateMicroService/app.py
+++ translateMicroService/app.py
@@ -11,16 +11,15 @@ DEEPL_API_KEY = "62bbde3e-4c12-4954-be15-45d4c358d069:fx"
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 app = Flask(__name__)
-CORS(app, resources={r"/traduzir": {"origins": "*"}})
+CORS(app, resources={r"/translate": {"origins": "*"}})
 
 # Validar idiomas
 def validar_idioma(idioma):
     return idioma.lower() in ["pt-br", "en"]
 
 # Traduzir texto usando a API do DeepL
-def traduzir_texto_deepl(texto, idioma_de_origem, idioma_de_destino):
+def traduzir_texto_deepl(texto, idioma_de_destino):
     try:
-        idioma_de_origem = "PT" if idioma_de_origem == "pt-br" else idioma_de_origem.upper()
         idioma_de_destino = "PT" if idioma_de_destino == "pt-br" else idioma_de_destino.upper()
 
 
@@ -29,7 +28,6 @@ def traduzir_texto_deepl(texto, idioma_de_origem, idioma_de_destino):
             data={
                 "auth_key": DEEPL_API_KEY,
                 "text": texto,
-                "source_lang": idioma_de_origem,
                 "target_lang": idioma_de_destino,
             },
         )
@@ -43,29 +41,25 @@ def traduzir_texto_deepl(texto, idioma_de_origem, idioma_de_destino):
         logging.error(f"Erro ao traduzir o texto: {texto}. Erro: {e}")
         return None
 
-@app.route("/traduzir", methods=["POST"])
+@app.route("/translate", methods=["POST"])
 def traduzir():
     try:
         dados = request.json
-        textos = dados.get("textos", [])
-        idioma_de_origem = dados.get("idioma_origem", "").lower()
-        idioma_de_destino = dados.get("idioma_destino", "").lower()
+        textos = dados.get("texts", [])
+        idioma_de_destino = dados.get("dest_language", "").lower()
 
         if not isinstance(textos, list) or not all(isinstance(texto, str) for texto in textos):
             return jsonify({"erro": "O campo 'textos' deve ser uma lista de strings."}), 400
 
-        if not validar_idioma(idioma_de_origem) or not validar_idioma(idioma_de_destino):
-            return jsonify({"erro": "Idiomas inválidos. Apenas 'pt-br' e 'en' são suportados."}), 400
-
+        
         traducoes = [
             {
-                "original": texto,
-                "traduzido": traduzir_texto_deepl(texto, idioma_de_origem, idioma_de_destino) or "Não foi possível traduzir este texto."
+                "translate": traduzir_texto_deepl(texto, idioma_de_destino) or "Não foi possível traduzir este texto."
             }
             for texto in textos
         ]
 
-        return jsonify({"traducoes": traducoes})
+        return jsonify({"translates": traducoes})
 
     except Exception as e:
         logging.error(f"Erro geral no servidor: {e}")
